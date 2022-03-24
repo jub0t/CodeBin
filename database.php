29e4462b584ee36b496788309e42aae4ccc24235
JUB0T <jforeverything2007@gmail.com>
2022-03-24 16:19:46 +0100

    update march

--- database.php
+++ database.php
@@ -4,7 +4,7 @@
 ************************************************************************************************* */
 
 $SQL = new mysqli("localhost", "root", "", "test");
-$SQL->query('CREATE TABLE `pastes` (
+$SQL->query('CREATE TABLE IF NOT EXISTS `pastes` (
   `created` varchar(255) NOT NULL,
   `id` varchar(255) PRIMARY KEY,
   `name` tinytext NOT NULL,
@@ -12,15 +12,16 @@ $SQL->query('CREATE TABLE `pastes` (
   `mark` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
 
-function toID($length) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return strtoupper($randomString);
-} 
+function toID($length)
+{
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $charactersLength = strlen($characters);
+  $randomString = '';
+  for ($i = 0; $i < $length; $i++) {
+    $randomString .= $characters[rand(0, $charactersLength - 1)];
+  }
+  return strtoupper($randomString);
+}
 
 if ($SQL->connect_error) {
   $CONNECTECTION_ERROR = $SQL->connet_error;
